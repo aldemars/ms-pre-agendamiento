4ff704dc86fcc90066b75f30b1c014ff58f9cf25
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-04-07 16:40:26 -0400

    ADD dependency injection on startup class

--- Repository/IScheduledCalendarBlockRepository.cs
+++ Repository/IScheduledCalendarBlockRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using ms_pre_agendamiento.Models;
 
 namespace ms_pre_agendamiento
 {
--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,8 @@ namespace ms_pre_agendamiento
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IScheduledCalendarBlockRepository, ScheduledCalendarBlockRepository>();
+            services.AddTransient<ICalendarAvailabilityService, CalendarAvailabilityService>();
             services.AddControllers();
         }
 
