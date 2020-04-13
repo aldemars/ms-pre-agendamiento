6e479ef2681641a91369ccad39318db4800b8cc1
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-04-13 15:49:10 -0400

    [Anna/Gonzalo] Refactor to sln structure

---
+++ ms-pre-agendamiento.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ms_pre_agendamiento.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
