cb628991017e1b9f6891ef7effafe5a238b35d7d
Aldemar Santamaria <laasantamaria@MacBook-Pro-de-Aldemar.local>
2020-04-22 12:58:27 -0400

    login fix

--- ms-pre-agendamiento/Controllers/Login.cs
+++ ms-pre-agendamiento/Controllers/Login.cs
@@ -2,47 +2,60 @@ using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using ms_pre_agendamiento.DTO;
 using ms_pre_agendamiento.Repository;
 
 namespace ms_pre_agendamiento.Controllers
 {
+    [AllowAnonymous]
     [ApiController, Route("[controller]")]
     public class Login : ControllerBase
     {
-        private readonly SymmetricSecurityKey _mySecurityKey =
-            new SymmetricSecurityKey(Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaa"));
+        private readonly SymmetricSecurityKey _mySecurityKey;
 
         private readonly IUserRepository _userRepository;
+        
+        private readonly string _issuer;
 
-        public Login(IUserRepository userRepository)
+
+        public Login(IUserRepository userRepository, IConfiguration configuration)
         {
             _userRepository = userRepository;
+            var secret = configuration.GetSection("AppSettings")["Secret"];
+            _issuer = configuration.GetSection("AppSettings")["Issuer"];
+            _mySecurityKey =
+                new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] User user)
+        public IActionResult Post([FromBody] User userDto)
         {
-            Console.WriteLine(user);
-            if (!_userRepository.isValid(user)) return Unauthorized();
-            const string myIssuer = "http://pre-agendamiento-front.azurewebsites.net";
+            Models.User user = _userRepository.getUser(userDto);
+            if (user == null) return Unauthorized();
 
             var tokenHandler = new JwtSecurityTokenHandler();
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new[]
                 {
-                    new Claim(ClaimTypes.NameIdentifier, "fakeUserId"),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    new Claim(ClaimTypes.Name, user.Name),
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
-                Issuer = myIssuer,
+                Issuer = _issuer,
                 SigningCredentials = new SigningCredentials(_mySecurityKey, SecurityAlgorithms.HmacSha256Signature)
             };
 
+            HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, null, new AuthenticationProperties());
             var token = tokenHandler.CreateToken(tokenDescriptor);
-            return new ObjectResult(new LoginResponse(tokenHandler.WriteToken(token))) ;
+            user.Token = tokenHandler.WriteToken(token);
+            return new ObjectResult(user);
         }
     }
 }
