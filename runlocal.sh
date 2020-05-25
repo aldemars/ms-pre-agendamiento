d2bc6b20f3365851d442bfc108417b564778f7be
Aldemar Santamaria <laasantamaria@MacBook-Pro-de-Aldemar.local>
2020-05-25 15:16:11 -0400

    appconfig variable in runlocal script

--- runlocal.sh
+++ runlocal.sh
@@ -1,3 +1,3 @@
 export ConnectionStrings__database=$(sh az keyvault secret show --name 'ConnectionStrings' --vault-name 'dev-pre-agendamiento' --query value  | tr -d \")
-export ConnectionStrings__AppConfig=$(sh az keyvault secret show --name 'appConfiguration' --vault-name 'dev-pre-agendamiento' --query value  | tr -d \")
+export ConnectionStrings__AppConfig=$(sh az keyvault secret show --name 'AppConfig' --vault-name 'dev-pre-agendamiento' --query value  | tr -d \")
 dotnet run --project ms-pre-agendamiento/ms-pre-agendamiento.csproj
